5aae474a9b643705ff0a8de5a5e356aa46be876d
cab-1729 <60106638+cab-1729@users.noreply.github.com>
2021-07-04 18:35:29 +0530

    Add files via upload

--- config/ranger/commands.py
+++ config/ranger/commands.py
@@ -1,62 +1,33 @@
-# This is a sample commands.py.  You can add your own commands here.
-#
-# Please refer to commands_full.py for all the default commands and a complete
-# documentation.  Do NOT add them all here, or you may end up with defunct
-# commands when upgrading ranger.
-
-# A simple command for demonstration purposes follows.
-# -----------------------------------------------------------------------------
-
-from __future__ import (absolute_import, division, print_function)
-
-# You can import any python module as needed.
-import os
-
-# You always need to import ranger.api.commands here to get the Command class:
+from pyperclip import copy
+from os import listdir
 from ranger.api.commands import Command
-
-
-# Any class that is a subclass of "Command" will be integrated into ranger as a
-# command.  Try typing ":my_edit<ENTER>" in ranger!
-class my_edit(Command):
-    # The so-called doc-string of the class will be visible in the built-in
-    # help that is accessible by typing "?c" inside ranger.
-    """:my_edit <filename>
-
-    A sample command for demonstration purposes that opens a file in an editor.
-    """
-
-    # The execute method is called when you run this command in ranger.
+def linux2windows(lin_path):#converts a linux path to the windows equivalent
+    if not lin_path.startswith("/mnt/"):#file not in windows
+        print('path cannot be converted to windows')
+        return ''
+    else:
+        return lin_path[5].upper()+':'+lin_path[6:].replace('/','\\')
+class yank(Command):
     def execute(self):
-        # self.arg(1) is the first (space-separated) argument to the function.
-        # This way you can write ":my_edit somefilename<ENTER>".
-        if self.arg(1):
-            # self.rest(1) contains self.arg(1) and everything that follows
-            target_filename = self.rest(1)
-        else:
-            # self.fm is a ranger.core.filemanager.FileManager object and gives
-            # you access to internals of ranger.
-            # self.fm.thisfile is a ranger.container.file.File object and is a
-            # reference to the currently selected file.
-            target_filename = self.fm.thisfile.path
-
-        # This is a generic function to print text in ranger.
-        self.fm.notify("Let's edit the file " + target_filename + "!")
-
-        # Using bad=True in fm.notify allows you to print error messages:
-        if not os.path.exists(target_filename):
-            self.fm.notify("The given file does not exist!", bad=True)
-            return
-
-        # This executes a function from ranger.core.acitons, a module with a
-        # variety of subroutines that can help you construct commands.
-        # Check out the source, or run "pydoc ranger.core.actions" for a list.
-        self.fm.edit_file(target_filename)
-
-    # The tab method is called when you press tab, and should return a list of
-    # suggestions that the user will tab through.
-    # tabnum is 1 for <TAB> and -1 for <S-TAB> by default
-    def tab(self, tabnum):
-        # This is a generic tab-completion function that iterates through the
-        # content of the current directory.
-        return self._tab_directory_content()
+        option1=self.args[1]
+        under=self.fm.thisfile#item under cursor
+        #neutral yanks
+        if option1=='content':#content to clipboard
+            if under.is_directory:
+                copy('\n'.join(listdir(under.path)))
+            else:
+                with open(under.path,'r') as file:copy(file.read())
+        elif option1=='name':
+            copy(under.basename)
+        elif option1=='name_without_extension':
+            copy(under.basename[:under.basename.rindex('.')])
+        #linux yanks
+        elif option1=='linux':
+            option2=self.args[2]
+            if option2=='path':copy(under.path)
+            elif option2=='cwd':copy(self.fm.thisdir.path)
+        #windows yanks
+        elif option1=='windows':#windows yanks
+            option2=self.args[2]
+            if option2=='path':copy(linux2windows(under.path))
+            elif option2=='cwd':copy(linux2windows(self.fm.thisdir.path))
