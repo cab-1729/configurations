17b3daebdd7b07fb8c9f7b9e8a92d45c27bb4aba
cab-1729 <60106638+cab-1729@users.noreply.github.com>
2021-10-15 09:46:55 +0530

    Delete sound_toggle.sh

--- manjaro configs/tmux/sound_toggle.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-if [ "$(pacmd list-sinks | sed '65q;d')" == "	active port: <analog-output-headphones>" ];then
-	pacmd set-sink-port alsa_output.pci-0000_00_1f.3.analog-stereo analog-output-lineout
-else
-	pacmd set-sink-port alsa_output.pci-0000_00_1f.3.analog-stereo analog-output-headphones
-fi
