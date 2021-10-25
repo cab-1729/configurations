5e0b307afdbdee9e88c30a6b591e26b17467a4ea
cab-1729 <60106638+cab-1729@users.noreply.github.com>
2021-10-25 17:31:43 +0530

    Delete bashrc.sh

--- wsl config/dot config/bashrc.sh
+++
@@ -1,6 +0,0 @@
-set -o vi
-export EDITOR='nvim'
-PS1="\[\033[1;34m\]\w # \[\033[0m\]"
-set bell-style none
-PROMPT_COMMAND='if [[ "$bashrc" != "$PWD" && "$PWD" != "$HOME" && -e .bashrc ]]; then bashrc="$PWD"; . .bashrc; fi' #run local rc
-#export TERM=xterm-256color
